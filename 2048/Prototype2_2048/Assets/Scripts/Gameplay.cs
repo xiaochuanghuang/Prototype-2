2b37afe7f539f7e4e4b526fa25dbe67f14c952b0
u1067294 <u1067294@utah.edu>
2022-01-31 16:58:12 -0700

    Added Images

--- 2048/Prototype2_2048/Assets/Scripts/Gameplay.cs
+++ 2048/Prototype2_2048/Assets/Scripts/Gameplay.cs
@@ -1,6 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public struct EmptySpace
+{
+    int rowPosition { get; set; }
+    int colPosition { get; set; }
+
+    public EmptySpace(int row, int col) : this()
+    {
+        this.colPosition = col;
+        this.rowPosition = row;
+    }
+}
 
 public class Gameplay 
 {
@@ -18,6 +31,7 @@ public class Gameplay
         Right
     }
 
+  
     public Gameplay()
     {
         space = new int[4, 4];
@@ -27,32 +41,114 @@ public class Gameplay
 
     private void GoUp()
     {
+        for (int col = 0; col < 4; col++)
+        {
+            for (int row = 0; row <= 3; row++)
+            {
+                this.combineArray[row] = this.space[col, row];
+            }
+            Combined();
 
+            for (int row = 0; row <= 3; row++)
+            {
+                this.space[row, col] = combineArray[row];
+            }
+        }
     }
 
     private void GoDown()
     {
+        for (int col = 0; col < 4; col++)
+        {
+            for (int row = 3; row >= 0; row--)
+            {
+                this.combineArray[3 - row] = this.space[col, row];
+            }
+            Combined();
 
+            for (int row = 3; row >= 0; row--)
+            {
+                this.space[row, col] = combineArray[3-row];
+            }
+        }
     }
     private void GoLeft()
     {
+        for (int col = 0; col < 4; col++)
+        {
+            for (int row = 0; row <= 3; row++)
+            {
+                this.combineArray[row] = this.space[col, row];
+            }
+            Combined();
+
+            for (int row = 0; row <= 3; row++)
+            {
+                this.space[col, row] = combineArray[row];
+            }
+        }
 
     }
     private void GoRight()
     {
+        for(int col = 0; col < 4;col ++)
+        {
+            for(int row = 3; row >=0;row --)
+            {
+                this.combineArray[3 - row] = this.space[col, row];
+            }
+            Combined();
 
+            for (int row = 3; row >= 0; row--)
+            {
+                this.space[col, row] = combineArray[3-row];
+            }
+        }
+    }
+    private void Combined()
+    {
+        removeZeroElement();
+
+        for(int i = 0; i<combineArray.Length-1;i++)
+        {
+            if(combineArray[i] == combineArray[i+1])
+            {
+                combineArray[i] += combineArray[i + 1];
+                combineArray[i + i] = 0;   
+            }
+        }
+        removeZeroElement();
+    }
+
+    private void removeZeroElement()
+    {
+        Array.Clear(RemoveEmptyElement, 0, 4);
+        int number = 0;
+        for(int i = 0; i < combineArray.Length;i++)
+        {
+            if(combineArray[i] != 0)
+            {
+                RemoveEmptyElement[number++] = combineArray[i];
+            }
+           
+        }
+        RemoveEmptyElement.CopyTo(combineArray, 0);
     }
     public void Move(Movement d)
     {
         switch(d)
         {
             case Movement.Up:
+                GoUp();
                 break;
             case Movement.Down:
+                GoDown();
                 break;
             case Movement.Left:
+                GoLeft();
                 break;
             case Movement.Right:
+                GoRight();
                 break;
         }
 
